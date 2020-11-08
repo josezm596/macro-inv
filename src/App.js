3e6f4cbc4a4ac1521b5766ef6384e34fc0140ca5
josezm596 <70865573+josezm596@users.noreply.github.com>
2020-11-08 14:18:46 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
   return (
     <div className="App">
@@ -10,12 +14,66 @@ function App() {
         macro inv
         </p>
         <nav>
-<ul>
-<li><a href="Inventarios.html" >Inventarios</a></li>
-<li><a href="Finanzas.html" >Finanzas</a></li>
-<li><a href="Usuarios.html" >Usuarios</a></li>
-<li><a href="Exportar.html" >Exportar</a></li>
-<li><a href="Facturacion.html" >Facturacion</a></li>
+<ul class="nav">
+<li><a href= "" >Inventarios</a>
+<li><a href="">Alta de Material</a>
+</li>
+<li><a href="">Baja de Material</a>
+</li>
+<li><a href="">Acomodo de Material</a>
+</li>
+
+</li>
+<li><a href="Finanzas.html" >Finanzas</a>
+<li><a href="">Clientes</a>
+</li>
+
+<li><a href="">Proveedores</a>
+</li>
+<li><a href="">Descuentos</a>
+</li>
+
+
+
+</li>
+<li><a href="Usuarios.html" >Usuarios</a>
+<li><a href="">Clave y Contraseña</a>
+</li>
+<li><a href="">Numero de usuario</a>
+</li>
+<li><a href="">Movimiento Registrado</a>
+</li>
+
+
+
+
+</li>
+<li><a href="Exportar.html" >Exportar</a>
+<li><a href="">Exportar a Excel</a>
+</li>
+<li><a href="">Exportar a PDF</a>
+</li>
+
+
+
+
+
+</li>
+<li><a href="Facturacion.html" >Facturacion</a>
+<li><a href="">Desea Facturar?</a>
+</li>
+<li><a href="">Cancelar Factura</a>
+</li>
+<li><a href="">Facturar en Dolares</a>
+</li>
+
+
+
+
+
+
+
+</li>
 </ul>
 </nav>
         
---
+++ src/componentes/Inventarios.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Inventarios() {
+  return (
+    <div className="app">
+      <header className="app">
+      
+        
+        <nav>
+<ul>
+<li><a href="Inventarios.js" ></a></li>
+<li><a href="Finanzas.html" >Finanzas</a></li>
+<li><a href="Usuarios.html" >Usuarios</a></li>
+<li><a href="Exportar.html" >Exportar</a></li>
+<li><a href="Facturacion.html" >Facturacion</a></li>
+</ul>
+</nav>
+        
+      
+      </header>
+    </div>
+  );
+}
+
+export default Inventarios;

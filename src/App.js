61cd2d563d8f2d166cad218be224e7d29ddb69c1
josezm596 <70865573+josezm596@users.noreply.github.com>
2020-11-07 19:56:00 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        macro inv
+        </p>
+        <nav>
+<ul>
+<li><a href="Inventarios.html" >Inventarios</a></li>
+<li><a href="Finanzas.html" >Finanzas</a></li>
+<li><a href="Usuarios.html" >Usuarios</a></li>
+<li><a href="Exportar.html" >Exportar</a></li>
+<li><a href="Facturacion.html" >Facturacion</a></li>
+</ul>
+</nav>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+       Contraseña
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

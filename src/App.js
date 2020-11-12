e1400c16a7f261a2bb0ae129f4225f95679037a1
josezm596 <70865573+josezm596@users.noreply.github.com>
2020-11-11 22:59:07 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,23 +8,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
       
+      <header className="App-header">
+   
         <p>
         macro inv
         </p>
+
         <nav>
 <ul class="nav">
 <li><a href= "" >Inventarios</a>
+<ul class="submenu">
 <li><a href="">Alta de Material</a>
 </li>
 <li><a href="">Baja de Material</a>
 </li>
 <li><a href="">Acomodo de Material</a>
 </li>
+</ul>
 
 </li>
-<li><a href="Finanzas.html" >Finanzas</a>
+<li><a href="" >Finanzas</a>
+<ul class="submenu">
 <li><a href="">Clientes</a>
 </li>
 
@@ -33,10 +38,12 @@ function App() {
 <li><a href="">Descuentos</a>
 </li>
 
+</ul>
 
 
 </li>
-<li><a href="Usuarios.html" >Usuarios</a>
+<li><a href="" >Usuarios</a>
+<ul class="submenu">
 <li><a href="">Clave y Contraseña</a>
 </li>
 <li><a href="">Numero de usuario</a>
@@ -44,29 +51,31 @@ function App() {
 <li><a href="">Movimiento Registrado</a>
 </li>
 
-
+</ul>
 
 
 </li>
-<li><a href="Exportar.html" >Exportar</a>
+<li><a href="" >Exportar</a>
+<ul class="submenu">
 <li><a href="">Exportar a Excel</a>
 </li>
 <li><a href="">Exportar a PDF</a>
 </li>
-
+</ul>
 
 
 
 
 </li>
-<li><a href="Facturacion.html" >Facturacion</a>
+<li><a href="" >Facturacion</a>
+<ul class="submenu">
 <li><a href="">Desea Facturar?</a>
 </li>
 <li><a href="">Cancelar Factura</a>
 </li>
 <li><a href="">Facturar en Dolares</a>
 </li>
-
+</ul>
 
 
 
